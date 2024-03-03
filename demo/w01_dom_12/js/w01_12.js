c51e1e33f50740eb85400fb4e89e5c0be9ac0ffd
jimmyhua123 <212410012@o365.tku.edu.tw>
2024-03-03 21:07:15 +0800

    W02-P1: Show Tictactoe css

---
+++ demo/w01_dom_12/js/w01_12.js
@@ -0,0 +1,22 @@
+
+
+const userInput =document.querySelector('#input-number');
+
+const addBtn = document.querySelector('#btn-add');
+const subtractBtn = document.querySelector('#btn-subtract');
+const multiplyBtn = document.querySelector('#btn-multiply');
+const divideBtn = document.querySelector('#btn-divide');
+
+
+const currentCalculationOutput = document.querySelector('#current-calculation');
+const currentResultOutput = document.querySelector('#current-result');
+
+
+// console.log('userInput',userInput);
+ console.log('currentcalculation',currentCalculationOutput);
+ console.log('currentresult',currentResultOutput);
+const defaultResult =0;
+
+function add(){
+    const operand1 = 0;
+}
