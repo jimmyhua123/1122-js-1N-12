b00c2675b150dfa21337e429c1b55b263c9dbe4e
jimmyhua123 <212410012@o365.tku.edu.tw>
2024-05-15 15:02:07 +0800

    W12-P3: Do 3_json_people_xhr

--- demo/w12_ajax_12/1_simple-text/app.js
+++ demo/w12_ajax_12/1_simple-text/app.js
@@ -1,28 +1,22 @@
-const btn = document.querySelector(".btn");
-btn.addEventListener("click",() =>{
-    getData();
-} );
 
-function getData() {
-  const xhr = new XMLHttpRequest();
-  console.log("xhr0", xhr);
-  xhr.open("GET", "api/sample.txt");
-  console.log("xhr", xhr);
-  xhr.onreadystatechange = function () {
-    console.log("xhr", xhr);
-    if (xhr.readyState === 4 && xhr.status === 200) {
-      const text = document.createElement("p");
-      text.textContent = xhr.responseText;
-      document.body.appendChild(text);
-    } else {
-      console.log({
-        status: xhr.status,
-        text: xhr.statusText,
-      });
+const xhr = new XMLHttpRequest();
+console.log('xhr0',xhr);
+
+xhr.open('GET', 'api/sample.txt');
+console.log('xhr',xhr);
+xhr.onreadystatechange = function()  {
+    console.log('xhr',xhr);
+    if(xhr.readyState === 4 && xhr.status === 200) {
+       const text =document.createElement('p') ;
+       text.textContent=xhr.responseText;
+       document.body.appendChild(text);
+    }else{
+        console.log({
+            status: xhr.status,
+            text: xhr.statusText,
+        });
     }
-  };
-  xhr.send();
-}
+};
 
-console.log("hello");
-
+xhr.send();
+console.log('hello');
--- demo/w12_ajax_12/3_json_people/app.js
+++ demo/w12_ajax_12/3_json_people/app.js
@@ -1,19 +1,27 @@
 const btn = document.querySelector(".btn");
+const url="./api/people.json"
+
 btn.addEventListener("click",() =>{
-    getData();
+    getData(url);
 } );
 
-function getData() {
+function getData(url) {
   const xhr = new XMLHttpRequest();
   console.log("xhr0", xhr);
-  xhr.open("GET", "api/sample.txt");
+  xhr.open("GET",url );
   console.log("xhr", xhr);
   xhr.onreadystatechange = function () {
     console.log("xhr", xhr);
     if (xhr.readyState === 4 && xhr.status === 200) {
-      const text = document.createElement("p");
-      text.textContent = xhr.responseText;
-      document.body.appendChild(text);
+      const data=JSON.parse(xhr.responseText);
+      const displayData = data.map((item) => {
+            return `
+            <p>${item.name}</p>
+            `  ;
+      }).join('');
+      const element = document.createElement("div");
+      element.innerHTML = displayData;
+      document.body.appendChild(element); 
     } else {
       console.log({
         status: xhr.status,
