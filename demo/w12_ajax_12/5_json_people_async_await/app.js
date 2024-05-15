93b1bc6e7388d3abc331fce9ea42b4fb641f4849
jimmyhua123 <212410012@o365.tku.edu.tw>
2024-05-15 15:23:44 +0800

    ### W12-P5: Do 3_json_people_async_await

---
+++ demo/w12_ajax_12/5_json_people_async_await/app.js
@@ -0,0 +1,71 @@
+const btn = document.querySelector(".btn");
+const url = "./api/people.json";
+
+btn.addEventListener("click", () => {
+  fetchDataAsync(url);
+});
+
+const  fetchDataAsync = async(url)=> {
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    console.log("data", data);
+    displayItems(data);
+  }catch (error) {
+    console.log(error);
+  }
+}
+
+function fetchData(url) {
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      console.log("data", data);
+      displayItems(data);
+    })
+    .catch((error) => console.log(error));
+}
+
+function displayItems(data) {
+  const displayData = data
+    .map((item) => {
+      return `
+            <p>${item.name}</p>
+            `;
+    })
+    .join("");
+  const element = document.createElement("div");
+  element.innerHTML = displayData;
+  document.body.appendChild(element);
+}
+
+function getData(url) {
+  const xhr = new XMLHttpRequest();
+  console.log("xhr0", xhr);
+  xhr.open("GET", url);
+  console.log("xhr", xhr);
+  xhr.onreadystatechange = function () {
+    console.log("xhr", xhr);
+    if (xhr.readyState === 4 && xhr.status === 200) {
+      const data = JSON.parse(xhr.responseText);
+      const displayData = data
+        .map((item) => {
+          return `
+            <p>${item.name}</p>
+            `;
+        })
+        .join("");
+      const element = document.createElement("div");
+      element.innerHTML = displayData;
+      document.body.appendChild(element);
+    } else {
+      console.log({
+        status: xhr.status,
+        text: xhr.statusText,
+      });
+    }
+  };
+  xhr.send();
+}
+
+console.log("hello");
