fbab150aa6a88f0b7de067af832934438470d8fb
jimmyhua123 <212410012@o365.tku.edu.tw>
2024-06-05 15:22:46 +0800

    W14-P2: Do products_localJson_xx.js to get local json data (/api/productsData.json)

--- demo/w15_products_12/js/products_localJson_12.js
+++ demo/w15_products_12/js/products_localJson_12.js
@@ -1,12 +1,49 @@
-import { products_xx, all_products_xx } from './p1_data_xx.js';
+// import { products_xx, all_products_xx } from './p1_data_xx.js';
 
+const url='./api/productsData.json';
+
+let products_xx = [];
 
 const productContainer = document.querySelector('.products-container');
 
+const fetchData = async () => {
+  try{
+    const response= await fetch(url)
+    const data = await response.json();
+    console.log("fetchdata", data);
+    return data;
+  }catch(error){
+    console.log(error);
+  }
+};
+
 console.log('products_xx', products_xx);
 
-const DisplayProducts = (products) => {};
+const displayProducts = (products) => {
+  let productsContent =products.map((product)=>{
+    const {name,price,image}=product.fields;
+    // const {id}=product;
+    return `
+    <div class="single-product">
+        <img
+          src=${image[0].url}
+          class="single-product-img img"
+          alt=${name}
+        />
+        <footer>
+          <h3 class="name">${name}</h3>
+          <span class="price">$${price}</span>
+        </footer>
+      </div>
+    `
+  }).join('');
+  productContainer.innerHTML=productsContent;
+};
 
-document.addEventListener('DOMContentLoaded', () => {
-  DisplayProducts(products_xx);
+
+document.addEventListener('DOMContentLoaded', async () => {
+  products_xx = await fetchData();
+  
+  displayProducts(products_xx);
+
 });
