68a27a2ce593f595056cff3645b54cfb2a52bfd5
jimmyhua123 <212410012@o365.tku.edu.tw>
2024-06-05 17:09:15 +0800

    W15-P2: Implement company filter functions, test for each company

--- demo/w15_products_12/js/products_supa_12.js
+++ demo/w15_products_12/js/products_supa_12.js
@@ -1,20 +1,25 @@
-//import { products_xx, all_products_xx } from './p1_data_xx.js';
 
 import { _supabase } from "./clientSupabase_12.js";
 
-let products_xx = [];
+let products_12 = [];
 
 const getProductsSupabase_xx = async () => {
   try{
-    let {data,error}= await _supabase.from('products_xx').select('*');
+    let {data,error}= await _supabase
+    .from('products_12')
+    .select('* company_12(*)');
     console.log('products data',data);
     return data;
   }catch(error){
     console.log(error);
   }
 };
+
+
+
 const productContainer = document.querySelector(".products-container");
-console.log("products_12", products_xx);
+const compantBtns = document.querySelectorAll(".company-btn");
+console.log("products_12", products_12);
 
 const displayProducts = (products) => {
   let productsContent =products.map((product)=>{
@@ -37,7 +42,30 @@ const displayProducts = (products) => {
   productContainer.innerHTML=productsContent;
 };
 
+compantBtns.forEach((btn)=>{
+  btn.addEventListener("click",async (e)=>{
+    const companyName = e.currentTarget.dataset.id;
+    console.log("companyName",companyName);
+    if(companyName==="all"){
+      products_12 =await getProductsSupabase_xx();
+    }else{
+      let {data:company,error1}= await _supabase
+      .from('company_12')
+      .select('id')
+      .eq('name',companyName);
+      console.log("companydata",company[0].id);
+      let {data,error2}= await _supabase
+      .from('products_12')
+      .select('*')
+      .eq('companyId',company[0].id);
+      console.log(`${companyName} products`,data);
+      products_12=data;
+    }
+    displayProducts(products_12);
+  });
+});
+
 document.addEventListener("DOMContentLoaded", async() => {
-  products_xx =await  getProductsSupabase_xx();
-  displayProducts(products_xx);
+  products_12 =await  getProductsSupabase_xx();
+  displayProducts(products_12);
 });
